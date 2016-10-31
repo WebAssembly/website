2ef1f48dd82a557a04b3ef04cf9bb5ba9bca15c2
Seth Thompson <s3th.thompson@gmail.com>
2016-10-30 22:30:25 -0700

    fix underscored links

--- _plugins/link_converter.rb
+++ _plugins/link_converter.rb
@@ -4,9 +4,25 @@ module LocalMdLinksToHtml
       site.pages.each { |p| rewrite_links(p) }
     end
     def rewrite_links(page)
-      page.content = page.content.gsub(/\[([^\]]*)\]\(([^:\)]*)\.md(#[^\)\s]*)?(?: "[^"\)]*")?\)/, '[\1](../\2/\3)')
-      page.content = page.content.gsub(/\[([^\]]*)\]: ([^:\)]*)\.md(#[^\)]*)?/, '[\1]: ../\2/\3')
-      page.content = page.content.gsub(/\[([^\]]*)\]: <([^:\)]*)\.md(#[^\)]*)?>/, '[\1]: <../\2/\3>')
+      page.content = page.content.gsub(/\[([^\]]*)\]\(([^:\)]*)\.md(#[^\)\s]*)?(?: "[^"\)]*")?\)/) do
+        slug = underscore($2)
+        "[#{$1}](../#{slug}/#{$3})"
+      end
+      page.content = page.content.gsub(/\[([^\]]*)\]: ([^:\)]*)\.md(#[^\)]*)?/) do
+        slug = underscore($2)
+        "[#{$1}]: ../#{slug}/#{$3}"
+      end
+      page.content = page.content.gsub(/\[([^\]]*)\]: <([^:\)]*)\.md(#[^\)]*)?>/) do
+        slug = underscore($2)
+        "[#{$1}]: <../#{slug}/#{$3}>"
+      end
+    end
+
+    def underscore(str)
+      str.gsub(/::/, '/').
+      gsub(/([A-Z]+)([A-Z][a-z])/,'\1-\2').
+      gsub(/([a-z\d])([A-Z])/,'\1-\2').
+      downcase
     end
   end
 end
