66d319bf2d528363568f2110f75bc970c5cf79a1
Seth Thompson <s3th.thompson@gmail.com>
2016-10-31 00:27:34 -0700

    sort docs

--- _plugins/auto_titles.rb
+++ _plugins/auto_titles.rb
@@ -8,6 +8,41 @@ module AutoTitles
 
     def generate_title(page)
       page.data["title"] = guess_title(page)
+      page.data["weight"] = assign_weight(page.data["title"])
+    end
+
+    # TODO:s3ththompson avoid hard-coding titles
+    def assign_weight(title)
+      order = [
+        "WebAssembly High-Level Goals",
+        "JavaScript API",
+        "Binary Encoding",
+        "Text Format",
+        "Semantics",
+        "Modules",
+        "FAQ",
+        "Design Rationale",
+        "Minimum Viable Product",
+        "Features to add after the MVP",
+        "Portability",
+        "Security",
+        "Nondeterminism in WebAssembly",
+        "Use Cases",
+        "Guide for C/C++ developers",
+        "Web Embedding",
+        "Non-Web Embeddings",
+        "Feature Test",
+        "Tooling support",
+        "GC / DOM / Web API Integration :unicorn:",
+        "JIT and Optimization Library",
+        "Dynamic linking"
+      ]
+      if order.include?(title)
+        order.index(title)
+      else
+        999
+      end
+
     end
 
     def guess_title(page)
