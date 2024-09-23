0a60e1ac0cf2129894f5a020491be952f9043727
Thomas Steiner <tomac@google.com>
2024-09-23 12:03:19 +0200

    Polyfill `Object.groupBy()`
    Fixes https://github.com/GoogleChromeLabs/wasm-feature-detect/issues/82.

--- features.js
+++ features.js
@@ -61,6 +61,30 @@
     tBody
   );
 
+  /*! groupby-polyfill. MIT License. Jimmy Wärting <https://jimmy.warting.se/opensource> */
+
+  /**
+   * Groups elements from an iterable into an object based on a callback function.
+   *
+   * @template T, K
+   * @param {Iterable<T>} iterable - The iterable to group.
+   * @param {function(T, number): K} callbackfn - The callback function to
+   * determine the grouping key.
+   * @returns {Object.<string, T[]>} An object where keys are the grouping keys
+   * and values are arrays of grouped elements.
+   *
+   * This was introduced because of https://github.com/GoogleChromeLabs/wasm-feature-detect/issues/82.
+   */
+  Object.groupBy ??= function groupBy (iterable, callbackfn) {
+    const obj = Object.create(null)
+    let i = 0
+    for (const value of iterable) {
+      const key = callbackfn(value, i++)
+      key in obj ? obj[key].push(value) : (obj[key] = [value])
+    }
+    return obj
+  }
+
   let featureGroups = Object.groupBy(
     Object.entries(features).map(([name, feature]) => Object.assign(feature, { name })),
     f => f.phase,
